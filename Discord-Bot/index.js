b7e20205a6890ea9e2ed57f197b259857b6c1bd4
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-04-26 12:56:50 -0600

    AI / Economy pt.1

--- Discord-Bot/index.js
+++ Discord-Bot/index.js
@@ -4,6 +4,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes, InteractionResponseType } = require('discord-api-types/v9');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { Configuration, OpenAIApi } = require("openai");
 
 // Process errors
 process.on('uncaughtException', function (error) {
@@ -23,6 +24,12 @@ console.log('MongoDB Connected')
 // Define Colors
 const mainHex = '#d79a61'
 
+// Open AI Connection
+const aiConfig = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(aiConfig)
+
 // Write Command Definitions
 const commands = [
     { name: 'help', description: 'Get a list of all Seeds commands' },
@@ -31,7 +38,11 @@ const commands = [
     { name: 'kick', description: 'Kick a member from the server', options: [{ name: 'user', description: 'The person you want to kick', required: true, type: Constants.ApplicationCommandOptionTypes.USER }, { name: 'reason', description: 'The reason the person is being kicked', required: false, type: Constants.ApplicationCommandOptionTypes.STRING }] },
     { name: 'warn', description: 'Warn a member in the server', options: [{ name: 'user', description: 'The person you want to warn', required: true, type: Constants.ApplicationCommandOptionTypes.USER }, { name: 'reason', description: 'The reason for the warn', required: true, type: Constants.ApplicationCommandOptionTypes.STRING }] },
     { name: 'cases', description: 'Check a members cases', options: [{ name: 'user', description: 'The person whos cases you want', required: true, type: Constants.ApplicationCommandOptionTypes.USER }] },
-    { name: 'deletecase', description: 'Delete a specific case', options: [{ name: 'case', description: 'The case ID that you want to delete', required: true, type: Constants.ApplicationCommandOptionTypes.INTEGER }] }
+    { name: 'deletecase', description: 'Delete a specific case', options: [{ name: 'case', description: 'The case ID that you want to delete', required: true, type: Constants.ApplicationCommandOptionTypes.INTEGER }] },
+    { name: 'friend', description: 'Talk to an AI friend', options: [{ name: 'message', description: 'What you want to say to your friend', required: true, type: Constants.ApplicationCommandOptionTypes.STRING }] },
+    { name: 'tsh', description: 'Make a 2 sentence horror story based on a given topic', options: [{ name: 'topic', description: 'The topic of the horror story', required: true, type: Constants.ApplicationCommandOptionTypes.STRING }] },
+    { name: 'balance', description: 'Check you SeedCoin balance' },
+    { name: 'beg', description: 'Beg Seeds for some coins (has varrying results)' }
 ]
 
 // Register slash commands
@@ -99,6 +110,24 @@ client.on('interactionCreate', async interaction => {
     if (commandName == 'deletecase') {
         const caseId = options.getInteger('case')
         await deletecaseCmd(user,guild,interaction,caseId)
+    }
+
+    if (commandName == 'friend') {
+        const prompt = options.getString('message')
+        await aiCmd(user,guild,interaction,prompt)
+    }
+
+    if (commandName == 'tsh') {
+        const topic = options.getString('topic')
+        await tshCmd(user,guild,interaction,topic)
+    }
+
+    if (commandName == 'balance') {
+        await balanceCmd(user,guild,interaction)
+    }
+
+    if (commandName == 'beg') {
+        await begCmd(user,guild,interaction)
     }
 })
 
@@ -173,7 +202,8 @@ function helpCmd(user,guild,interaction) {
     .setColor(mainHex)
     .setDescription('All Seeds commands use the prefix `\`/`\`\n\n[parameter] = Required\n{parameter} = Optional\n')
     .setFields([
-        { name: 'Moderation:', value: '`\`/ban [user] {reason}`\`, `\`/unban [user]`\`, `\`/kick [user] {reason}`\`, `\`/warn [user] [reason]`\`, `\`/cases [user]`\`, `\`/deletecase [case]`\`', inline: false }
+        { name: 'Moderation:', value: '`\`/ban [user] {reason}`\`, `\`/unban [user]`\`, `\`/kick [user] {reason}`\`, `\`/warn [user] [reason]`\`, `\`/cases [user]`\`, `\`/deletecase [case]`\`', inline: false },
+        { name: 'Fun:', value: '`\`/friend [message]`\`, `\`/tsh [topic]`\`', inline: false }
     ])
 
     interaction.reply({
@@ -443,7 +473,7 @@ async function casesCmd(user,guild,interaction,caseUser) {
             })
         } else {
             const embed = new MessageEmbed()
-            .setTitle(`${caseUser.tag}'s Cases`)
+            .setTitle(caseUser.tag + "'s Cases")
             .setDescription(doc.length + ' Cases')
             .setColor(mainHex)
 
@@ -478,6 +508,135 @@ async function deletecaseCmd(user,guild,interaction,caseId) {
             cmdRun(user,cmdName)
         })
     }
+}
+
+// AI command
+async function aiCmd(user,guild,interaction,prompt) {
+    const cmdName = 'friend'
+
+    const completion = await openai.createCompletion("text-davinci-002", {
+        prompt: "You: " + prompt + "\n Friend: ",
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+        stop: ["You:"],
+    })
+
+    if (completion.data.choices[0].text != '') {
+        const embed = new MessageEmbed()
+        .setTitle('AI Friend')
+        .setDescription('You: ' + prompt )
+        .addField('AI:', completion.data.choices[0].text)
+        .setColor(mainHex)
+
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+
+    cmdRun(user,cmdName)
+}
+
+// Two sentence horror
+async function tshCmd(user,guild,interaction,topic) { 
+    const cmdName = 'tsh'
+
+    const res = await openai.createCompletion("text-davinci-002", {
+        prompt: `Topic: ${topic}\nTwo-Sentence Horror Story:`,
+        temperature: 0.8,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+    })
+
+    if (res.data.choices[0].text != '') {
+        const embed = new MessageEmbed()
+        .setTitle('Two Sentence Horror | Topic: ' + topic)
+        .setDescription(res.data.choices[0].text)
+        .setColor(mainHex)
+
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+
+    cmdRun(user,cmdName)
+}
+
+// Economy System 
+// balance command
+async function balanceCmd(user,guild,interaction) {
+    const cmdName = 'balance'
+
+    var collection = db.collection('economy')
+    const doc = await collection.findOne({ userId: user.id, guildId: guild.id })
+
+    if (doc == null) {
+        const embed = new MessageEmbed()    
+        .setTitle(user.tag + ' has no SeedCoin')
+        .setColor(mainHex)
+        
+        interaction.reply({
+            embeds: [embed]
+        })
+    } else {
+        const coins = doc.coins
+        const embed = new MessageEmbed()
+        .setTitle('Bank Balance')
+        .setDescription('<@' + user.id + '> has **<:fuck_coin:824720614543196220> ' + coins + ' SeedCoins**')
+        .setColor(mainHex)
+
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+    cmdRun(user,cmdName)
+}
+
+// beg command
+async function begCmd(user,guild,interaction) {
+    const cmdName = 'beg'
+
+    const coinsAdd = Math.floor(Math.random() * 100)
+
+    var collection = db.collection('economy')
+    const doc = await collection.findOne({ userId: user.id, guildId: guild.id })
+
+    if (doc == null) {
+        const userData = {
+            userId: user.id,
+            guildId: guild.id,
+            coins: coinsAdd
+        }
+
+        await collection.insertOne(userData)
+
+        const embed = new MessageEmbed()
+        .setTitle('Ugh.. I guess')
+        .setDescription("Here's **<:fuck_coin:824720614543196220>" + coinsAdd + "**\n\nYour new balance is **<:fuck_coin:824720614543196220>" + coinsAdd + "**")
+        
+        interaction.reply({
+            embeds: [embed]
+        })
+    } else {
+        const oldBal = doc.coins
+        const newBal = oldBal + coinsAdd
+
+        await collection.updateOne({ userId: user.id, guildId: guild.id }, { $set: { coins: newBal }})
+
+        const embed = new MessageEmbed()
+        .setTitle('Ugh.. I guess')
+        .setDescription("Here's **<:fuck_coin:824720614543196220>" + coinsAdd + "**\n\nYour new balance is **<:fuck_coin:824720614543196220>" + newBal + "**")
+
+        interaction.reply({
+            embeds: [embed]
+        })
+    }
+
+    cmdRun(user,cmdName)
 }
 
 // Run Bot
