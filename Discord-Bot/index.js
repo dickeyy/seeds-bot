20712485a421d21b5dfb0af6d547c45db0647220
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-08-08 12:12:11 -0500

    Fixed Bug
    Added a handler for the shop command to handle if the user has no coins

--- Discord-Bot/index.js
+++ Discord-Bot/index.js
@@ -1286,9 +1286,15 @@ async function shopCmd(user,guild,interaction) {
     const doc = await collection.findOne({ userId: user.id, guildId: guild.id })
 
     const embed = new MessageEmbed()
-    .setTitle('Seed Shop | Balance: ' + doc.coins)
-    .setDescription('Buy any items with `\`/buy [item id]`\`')
-    .setFields([
+
+    if (doc == null) {
+        embed.setTitle('Seed Shop | Balance: 0')
+    } else {
+        embed.setTitle('Seed Shop | Balance: ' + doc.coins)
+    }
+
+    embed.setDescription('Buy any items with `\`/buy [item id]`\`')
+    embed.setFields([
         { name: 'Fishing Poles', value: '> `\`' + shopData.items.i_1.id + '`\` - ' + shopData.items.i_1.name + ': ' + shopData.items.i_1.description + ' Price: **' + shopData.items.i_1.price + '**', inline: false },
         { name: 'Pickaxes', value: '> `\`' + shopData.items.i_2.id + '`\` - ' + shopData.items.i_2.name + ': ' + shopData.items.i_2.description + ' Price: **' + shopData.items.i_2.price + '**\n' + '> `\`' + shopData.items.i_3.id + '`\` - ' + shopData.items.i_3.name + ': ' + shopData.items.i_3.description + ' Price: **' + shopData.items.i_3.price + '**\n' + '> `\`' + shopData.items.i_4.id + '`\` - ' + shopData.items.i_4.name + ': ' + shopData.items.i_4.description + ' Price: **' + shopData.items.i_4.price + '**\n' + '> `\`' + shopData.items.i_5.id + '`\` - ' + shopData.items.i_5.name + ': ' + shopData.items.i_5.description + ' Price: **' + shopData.items.i_5.price + '**\n' + '> `\`' + shopData.items.i_6.id + '`\` - ' + shopData.items.i_6.name + ': ' + shopData.items.i_6.description + ' Price: **' + shopData.items.i_6.price + '**\n', inline: false }
     ])
