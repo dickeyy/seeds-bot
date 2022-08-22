6a3a8404ae0e8d2e048d376d4de409ce9f4b2116
ZerohRaG3 <kyledickey02@gmail.com>
2022-08-22 07:50:00 -0600

    Bro these colors dont work

---
+++ react-web/src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from './theme'
+
+// Pages
+import AdminGuildsPage from './pages/Admin-Guilds';
+import AdminCmdsPage from './pages/Admin-Commands';
+import AdminEconPage from './pages/Admin-Economy';
+
+// root.render(
+//   <React.StrictMode>
+//     <HomePage />
+//   </React.StrictMode>
+// );
+
+export default function App() {
+    return (
+        <BrowserRouter>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Routes>
+            <Route path="/" element={<AdminGuildsPage />} />
+            <Route path="/admin/guilds" element={<AdminGuildsPage />} />
+            <Route path="/admin/commands" element={<AdminCmdsPage />} />
+            <Route path="/admin/economy" element={<AdminEconPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
--- react-web/src/index.js
+++ react-web/src/index.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import { ColorModeScript } from '@chakra-ui/react';
-import theme from './theme'
+import { ColorModeScript } from "@chakra-ui/react";
+import * as React from "react";
+import * as ReactDOM from 'react-dom/client'
 
-// Pages
-import AdminGuildsPage from './pages/Admin-Guilds';
-import AdminCmdsPage from './pages/Admin-Commands';
-import AdminEconPage from './pages/Admin-Economy';
+import App from "./app";
+import theme from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <HomePage />
-//   </React.StrictMode>
-// );
-
-root.render(
-  <BrowserRouter>
-  <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <Routes>
-      <Route path="/" element={<AdminGuildsPage />} />
-      <Route path="/admin/guilds" element={<AdminGuildsPage />} />
-      <Route path="/admin/commands" element={<AdminCmdsPage />} />
-      <Route path="/admin/economy" element={<AdminEconPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <>
+    {/* 👇 Here's the script */}
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </>,
+)
