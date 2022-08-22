c76bfa07aa75f3c483912be525998d9fce17f000
ZerohRaG3 <kyledickey02@gmail.com>
2022-08-22 10:08:08 -0600

    idk

--- react-web/api/routes/admin-fetch-economy.js
+++ react-web/api/routes/admin-fetch-economy.js
@@ -4,13 +4,17 @@ const econ = require("express").Router();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Connect to mongo
-const MongoUri = 'mongodb+srv://kyledickey:DTD4NjVU3sYhttU@cluster0.j548n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const mClient = new MongoClient(MongoUri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-mClient.connect();
-const db = mClient.db('main');
-console.log('MongoDB Connected')
+function connectDb() {
+    const MongoUri = 'mongodb+srv://kyledickey:DTD4NjVU3sYhttU@cluster0.j548n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+    const mClient = new MongoClient(MongoUri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    mClient.connect();
+    const db = mClient.db('main');
+    return db
+}
 
 econ.get("/admin/fetch-economy", async function (req, res) {
+    const db = connectDb();
+
     db.collection('economy').find({}).toArray((err, result) => {
             if (err) throw err;
             res.send(result)
