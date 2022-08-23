88430550d389d853eee8c27bee9ae9fdf616417f
ZerohRaG3 <kyledickey02@gmail.com>
2022-08-23 05:21:44 -0600

    Update Home.js

--- react-web/src/pages/Home.js
+++ react-web/src/pages/Home.js
@@ -2,29 +2,31 @@ import '../css/App.css';
 import * as React from 'react'
 import { ChakraProvider, Table, theme,  ThemeProvider, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer, Box, Stat, StatLabel, StatNumber, StatHelpText, StatArrow, StatGroup, Divider, Button, useToast, Skeleton, ColorModeScript, useColorMode, Heading, } from '@chakra-ui/react'
 import { FaDiscord } from 'react-icons/fa';
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams, useLocation } from "react-router-dom"
 
 // Components
 import Header from '../comps/Header';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 // import theme from '../theme';
 
-const discordUrl = 'https://discord.com/api/oauth2/authorize?client_id=968198214450831370&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=email%20identify%20guilds%20guilds.join'
+const discordUrl = 'https://discord.com/api/oauth2/authorize?client_id=968198214450831370&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=token&scope=identify%20email%20guilds'
 
 function HomePage() {
 
     const [isLoading, setLoading] = React.useState(false)
 
-    let [searchParams, setSearchParams] = useSearchParams()
-    const code = searchParams.get("code")
+    const location = useLocation();
+    const params = new URLSearchParams(location.hash);
+    const token = params.get('access_token');
+    const tokenType = params.get('token_type');
 
     React.useEffect(() => {
-        if (code !== null) {
+        if (token !== null) {
             setLoading(true)
 
-            authUser(code)
+            authUser(token, tokenType)
         }
-    })
+    }, [])
 
   return (
     <Box w='20vw' theme={theme} flexDirection="column">
@@ -43,8 +45,16 @@ function HomePage() {
 
 export default HomePage;
 
-async function authUser(code) {
+async function authUser(token, tokenType) {
 
-    
+    const reqUrl = `https://dashboard.seedsbot.xyz/api/auth_login?access_token=${token}&token_type=${tokenType}`
+
+    fetch(reqUrl)
+      .then(res => res.json())
+      .then(data => {
+        
+        console.log(data)
+      }
+      )
 
 }
