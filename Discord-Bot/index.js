c2708a8a87232edfced6fd3283ee4df45a441cf3
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-08-23 10:42:04 -0600

    Cron Job

--- Discord-Bot/index.js
+++ Discord-Bot/index.js
@@ -8,6 +8,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const osu = require('node-os-utils');
 const axios = require('axios').default
 const fs = require('fs')
+const cron = require('cron');
 
 // Process errors
 process.on('uncaughtException', async function (error) {
@@ -324,6 +325,34 @@ client.on('guildMemberRemove', async member => {
     const memCout = Number(guild.memberCount) - 1
     await collection.updateOne({ _id: member.guild.id }, { $set: { memberCount: memCout } })
 })
+
+// Cron Jobs
+let refreshHistory = new cron.CronJob('0 0 * * *', async () => {
+
+    // This runs every day at 10:30:00, you can do anything you want
+    const collection = db.collection('guilds');
+    const guilds =  await collection.find({}).toArray()
+
+    var gCount = 0
+    var mCount = 0
+    gCount = client.guilds.cache.size + gCount
+
+    for (var i = 0; i < guilds.length; i++) {
+        const guild = guilds[i]
+        const server = client.guilds.cache.get(guild.id)
+        if (server !== undefined) {
+            mCount = server.memberCount + mCount
+        }
+    }
+
+    const hisDataColl = db.collection('historicalData')
+    await hisDataColl.updateOne({ type: 'guildData' }, { $set: { guildCount: gCount, userCount: mCount } })
+
+    console.log('Updated historical data')
+
+});
+
+refreshHistory.start();
 
 // bot functions
 // Command run
