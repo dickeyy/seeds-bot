e59e878d5b254adabe3cd600bbc32fb6ff62c4b7
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-01-03 04:27:26 -0700

    Fixed ban cmds

--- main-bot/events/guildBanAdd.js
+++ main-bot/events/guildBanAdd.js
@@ -19,10 +19,10 @@ const guildBanAddEvent = async (ban) => {
 
             const webhookClient = new WebhookClient({ url: doc.webhookUrls.server });
 
-            const auditLog = await ban.guild.fetchAuditLogs({
-                limit: 1,
-                type: AuditLogEvent.MemberBanAdd,
-            });
+            // const auditLog = await ban.guild.fetchAuditLogs({
+            //     limit: 1,
+            //     type: AuditLogEvent.MemberBanAdd,
+            // });
 
             const embed = new MessageEmbed()
             .setTitle('Member Banned')
@@ -31,13 +31,13 @@ const guildBanAddEvent = async (ban) => {
             .setColor(lightRedHex)
             .setTimestamp()
 
-            if (auditLog) {
-                const { executor, target } = auditLog;
+            // if (auditLog) {
+            //     const { executor, target } = auditLog;
 
-                if (target.id === ban.user.id) {
-                    embed.addField('Banned By', `<@${executor.id}> (${executor.tag})`)
-                } 
-            }
+            //     if (target.id === ban.user.id) {
+            //         embed.addField('Banned By', `<@${executor.id}> (${executor.tag})`)
+            //     } 
+            // }
 
             webhookClient.send({
                 avatarURL: client.user.avatarURL(),
