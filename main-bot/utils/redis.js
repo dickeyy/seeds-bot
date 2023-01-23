778f05e7ecbfd8b4d063d35ea0df8268f46cb33c
ZerohRaG3 <kyledickey02@gmail.com>
2023-01-23 00:49:36 -0700

    Update redis.js

--- main-bot/utils/redis.js
+++ main-bot/utils/redis.js
@@ -1,15 +1,7 @@
 const dotenv = require('dotenv');
 const { createClient } = require('redis');
-const { log } = require('../functions/log');
 
 dotenv.config();
-
-// Process errors
-process.on('uncaughtException', async function (error) {
-    console.log('error', error.stack)
-
-    log('error', error.stack)
-});
 
 const redis = createClient({
     socket: {
