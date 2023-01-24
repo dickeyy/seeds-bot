eaa29c1e0080d95e73c2bc92932595d42c2cf969
ZerohRaG3 <kyledickey02@gmail.com>
2023-01-24 04:16:40 -0700

    Fixed colormode

--- next-app/pages/_document.js
+++ next-app/pages/_document.js
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import React from 'react';
 
 export default function Document() {
+
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+
+  React.useEffect(() => {
+    if (text === 'dark') {
+        toggleColorMode()
+    }
+  }, [])
+
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={'dark'} />
         <Main />
         <NextScript />
       </body>
