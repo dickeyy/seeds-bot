0dcc15a9413d42c5f7b6723dd3d9c98b3a140491
ZerohRaG3 <kyledickey02@gmail.com>
2023-01-25 13:20:24 -0700

    Fixed stats v5

--- next-app/pages/api/bot-stats.js
+++ next-app/pages/api/bot-stats.js
@@ -4,28 +4,26 @@ const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URI;
 const mClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+mClient.connect().then(() => {
+  //
+})
+
+const db = mClient.db("main");
+
 export default async function handler(req, res) {
   
-  mClient.connect().then(() => {
-    mClient.on('connect', () => {
-      const db = mClient.db("main");
-    
-      db.collection('guilds').find({}).toArray((err, result) => {
-        if (err) throw err;
+  db.collection('guilds').find({}).toArray((err, result) => {
+    if (err) throw err;
 
-        let userCount = 0;
-        result.forEach(guild => {
-          userCount += guild.memberCount
-        })
+    let userCount = 0;
+    result.forEach(guild => {
+      userCount += guild.memberCount
+    })
 
-        res.status(200).json({
-          guilds: result.length,
-          users: userCount
-        })
-      })
+    res.status(200).json({
+      guilds: (result.length - 1),
+      users: userCount
     })
   })
-
-  mClient.close();
 
 }
