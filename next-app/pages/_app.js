a5d7753a64e41d9ffca666477ae77178a3b85c3d
ZerohRaG3 <kyledickey02@gmail.com>
2023-02-09 18:07:04 -0700

    mirky analytics

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { ChakraProvider, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
+import Mirky from 'mirky'
 
 import theme from '../styles/theme.js'
 
@@ -10,7 +11,14 @@ export default function App({ Component, pageProps }) {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
 
+  const analytics = new Mirky()
+
   React.useEffect(() => {
+
+    analytics.init('6yYFCaXt7HX9mMDJL8Cp8D')
+
+    analytics.pageView()
+
     if (text === 'dark') {
         toggleColorMode
     }
