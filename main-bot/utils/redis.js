426a71a05e21f4f3db7661f21dae7760baa4563b
ZerohRaG3 <kyledickey02@gmail.com>
2023-10-09 17:40:49 -0600

    Update redis.js

--- main-bot/utils/redis.js
+++ main-bot/utils/redis.js
@@ -9,5 +9,18 @@ const redis = createClient({
     url: process.env.REDIS_URL
 })
 
+redis.on('error', (err) => {
+    if (err) throw err;
+    console.log('Redis disconnected')
+    log('info', 'Redis disconnected')
+    redis.connect()
+})
+
+redis.on('connect', (err) => {
+    if (err) throw err;
+    console.log('Redis connected')
+    log('info', 'Redis connected')
+})
+
 // Export the connect function
 exports.redis = redis;
