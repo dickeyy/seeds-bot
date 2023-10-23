b6394f098927966286b65127b80f35f04570a1b8
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-10-23 17:51:34 -0600

    Update discordElement.tsx

--- website/comps/hero/discordElement.tsx
+++ website/comps/hero/discordElement.tsx
@@ -5,7 +5,7 @@ import { FaHashtag, FaPlusCircle } from "react-icons/fa";
 export default function DiscordElement() {
 
     return (
-        <div className="bg-[#2f3336] w-full h-full p-4 rounded-lg justify-between flex flex-col">
+        <div className="bg-[#323337] w-full h-full p-4 rounded-lg justify-between flex flex-col">
             <div className="border-b border-zinc-600 flex flex-row pb-4 items-center">
                 <FaHashtag className="text-xl text-zinc-400 mr-2" />
                 <p className="text-zinc-400 font-medium">general</p>
