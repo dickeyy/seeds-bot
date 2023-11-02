e938b2ad06ab62d5674f03cc039ea4779a878883
dickeyy <kyledickey02@gmail.com>
2023-11-02 04:23:15 -0600

    aaaa

--- events-api/lib/sendMessage.ts
+++ events-api/lib/sendMessage.ts
@@ -1,7 +1,7 @@
 import logger from "./logger"
 
 export default async function sendMessage(data:any, webhookUrl:string) {
-    const req = await fetch("https://discord.com/api/webhooks/1167238448676081674/LPioAcAmqDcHfy7qTc6TktQvNnpasFbR0Bg9_bhjF2KIywN-kD3cJPWRy_Sm1Jc32piY", {
+    const req = await fetch(webhookUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
