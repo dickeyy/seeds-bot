a812ba4962ccc4c9b86bf21059b08fbe0f826f65
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-11-02 05:12:01 -0600

    removed file log transport

--- bun-bot/lib/logger.ts
+++ bun-bot/lib/logger.ts
@@ -33,8 +33,8 @@ const logger = winston.createLogger({
     defaultMeta: { service: 'seeds-bot' },
     transports: [
         new winston.transports.Console({ format: winston.format.simple() }),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' }),
+        // new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        // new winston.transports.File({ filename: 'logs/combined.log' }),
         whTransport,
         new AxiomTransport({
             dataset: config.axiom.dataset,
