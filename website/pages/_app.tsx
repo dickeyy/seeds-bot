52633f798dc914e1714d18d068d03cf5cb5e1c36
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-11-08 21:12:03 -0700

    add google analytics

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  )
 }
