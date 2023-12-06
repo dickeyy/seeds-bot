941becb6fe6d814b16366b368253379dc5027b0d
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2023-12-06 01:34:51 -0700

    posthog

---
+++ website/lib/posthog.ts
@@ -0,0 +1,3 @@
+import posthog, { PostHog } from 'posthog-js'
+
+export const hog:void | PostHog = posthog.init('phc_XGVCgPe5Tc6VEHP2qM71TM3VG3kanvO0OWU8SgZwKV0', { api_host: 'https://app.posthog.com' })
--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '@/styles/globals.css'
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import type { AppProps } from 'next/app'
+import { hog } from '../lib/posthog'
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  if (hog) hog
+
   return (
     <SessionProvider session={pageProps.session}>
-      <GoogleAnalytics trackPageViews />
+      {/* <GoogleAnalytics trackPageViews /> */}
       <Component {...pageProps} />
     </SessionProvider>
   )
